944eb95020beccb3b9a5f9d05fdb1d654145d762
Cadel Watson <cadel@cadelwatson.com>
2018-04-11 15:54:33 +1000

    Add signature verification skip to publish script

--- publish-package.ps1
+++ publish-package.ps1
@@ -9,6 +9,12 @@ Set-Location -Path ".\nuget-package"
 
 (Get-Content ".\Agridigital.System.Web.Http.nuspec.template").Replace('[VERSION]', $version) | Set-Content ".\Agridigital.System.Web.Http.nuspec"
 
+Set-Location -Path ".\lib"
+& 'C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\sn.exe' '-Vr' 'System.Web.Http.dll'
+& 'C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\sn.exe' '-Vr' 'System.Net.Http.Formatting.dll'
+
+Set-Location -Path "..\"
+
 & .\NuGet.exe 'pack' 'Agridigital.System.Web.Http.nuspec'
 & .\NuGet.exe 'push' ('Agridigital.System.Web.Http.{0}.nupkg' -f $version) $apikey '-Source' 'https://nuget.agridigital.io/api/v2/package'
 
